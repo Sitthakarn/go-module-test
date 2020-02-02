0a6dcde1c9a72a4242882b03b0a7b2fe90e0ea53
sitthakarn <sitthakarn.p@gmail.com>
2020-02-02 13:20:02 +0700

    hello: changes for v1.0.0

--- hello.go
+++ hello.go
@@ -1,11 +1,14 @@
 package hello
 
 import (
+	"fmt"
+
 	"rsc.io/quote/v3"
 )
 
 // Hello function
 func Hello() string {
+	fmt.Println("Test hello program in ver 1.0.0")
 	return quote.HelloV3()
 }
 
